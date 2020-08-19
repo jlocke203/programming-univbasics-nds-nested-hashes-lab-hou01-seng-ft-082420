cfc0e3d195218b411b2adb47bfda2747116f06a4
Jaden Locke <jadenlocke1113@gmail.com>
2020-08-19 16:04:08 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,6 +25,12 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+   nested_array = 
+  [{:name=>"Don Gately", :occupation=>"Live-in Staff"},
+        {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"},
+       {:name=>"Pat Monteseian", :occupation=>"Staff"},
+        {:name=>"Kate Gompert", :occupation=>"None"},
+        {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
   
   nested_array[0][:name]
 end
